a8267a03c3d1226115f0399e52b1bce5765805c5
giu176 <62891107+giu176@users.noreply.github.com>
2022-08-25 16:39:53 +0200

    Update print_circuit.js
    fixed

--- print_circuit.js
+++ print_circuit.js
@@ -1,5 +1,2 @@
-const loadR1cs = require("r1csfile").load
-
-loadR1cs("circuit.r1cs").then((r1cs) => {
-	console.log(r1cs);
-});
+const r1cs = require('r1csfile')
+r1cs.readR1cs("circuit.r1cs").then((r1cs)=>{console.log(r1cs)});
